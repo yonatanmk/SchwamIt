632ad07324b858211e165d5705ea11d2ca873269
Scott Ehrmann <s.s.ehrmann@gmail.com>
2017-01-18 10:14:53 -0500

    added item new and create controller methods, as well as new item view

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,21 @@
+class ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to @item
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description)
+  end
+end
