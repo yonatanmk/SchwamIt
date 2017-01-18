a33477db54b543895ced5150c169fe2a8a462ba2
Yonatan MK <yonatanmk@gmail.com>
2017-01-18 13:17:35 -0500

    made User model have many items

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
 
+  has_many :items
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
