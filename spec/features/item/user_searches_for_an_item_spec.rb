b0d6b5c73e67bdcd4d6b3a64f439497d55b4351b
Scott Ehrmann <s.s.ehrmann@gmail.com>
2017-01-25 14:48:36 -0500

    tests working

--- spec/features/item/user_searches_for_an_item_spec.rb
+++ spec/features/item/user_searches_for_an_item_spec.rb
@@ -2,47 +2,38 @@ require "rails_helper"
 
 feature "user visits index page" do
 
+
   before(:each) do
     user = FactoryGirl.create(:user)
     10.times { FactoryGirl.create(:item, user: user) }
     FactoryGirl.create(:item, title: "Dragonball Z", user: user)
+    FactoryGirl.create(:item, title: "Dragonball GT", user: user)
     visit items_path
   end
 
-  after(:each) do
-    DatabaseCleaner.clean
-  end
-
   scenario "user searches for a unique item" do
-    fill_in "search", with: "Pokemon2"
+    fill_in "search", with: "Dragonball Z"
     click_button "Search"
 
-    expect(page).to have_content "Pokemon2"
-    expect(page).to_not have_content "Pokemon1"
-    (3..10).each do |n|
-      expect(page).to_not have_content "Pokemon#{n}"
-    end
-    expect(page).to_not have_content "Dragonball Z"
+    expect(page).to_not have_content "Pokemon"
+    expect(page).to have_content "Dragonball Z"
   end
 
   scenario "user searches with a non-unique query" do
-    fill_in "search", with: "Pokemon"
+    fill_in "search", with: "Dragonball"
     click_button "Search"
 
-    (1..10).each do |n|
-      expect(page).to have_content "Pokemon#{n}"
-    end
-    expect(page).to_not have_content "Dragonball Z"
+    expect(page).to have_content "Dragonball Z"
+    expect(page).to have_content "Dragonball GT"
+    expect(page).to_not have_content "Pokemon"
   end
 
   scenario "user searches for a query with no results" do
     fill_in "search", with: "Power Rangers"
     click_button "Search"
 
-    (1..10).each do |n|
-      expect(page).to_not have_content "Pokemon#{n}"
-    end
-    expect(page).to_not have_content "Dragonball Z"
+    expect(page).to_not have_content "Pokemon"
+    expect(page).to_not have_content "Dragonball"
   end
 
   scenario "user does a case-insensitive search" do
