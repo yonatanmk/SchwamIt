6de2211dbe5d4e1b6976aa99443f74fd88981234
Scott Ehrmann <s.s.ehrmann@gmail.com>
2017-01-26 10:55:16 -0500

    api controller tests for items and votes

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,10 +1,6 @@
 class Api::V1::ReviewsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
-  def index
-
-  end
-
   def destroy
     @review = Review.find(params[:id])
     @review.destroy
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,6 +63,7 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include Warden::Test::Helpers
   config.include Devise::Test::IntegrationHelpers, type: :feature
+  config.include Devise::Test::ControllerHelpers, type: :controller
 
   config.before :each do
     ActionMailer::Base.deliveries.clear
